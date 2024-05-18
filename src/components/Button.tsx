982557a51b9749caae5b978e7d7f3ad9b81aa20a
Robert Crocker <rcrocker13@gmail.com>
2024-05-18 17:43:21 -0500

    Initialized Shadcn/ui

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { useFormStatus } from "react-dom";
+import { Button as ShadButton } from "@/components/ui/button";
 
 export const Button = () => {
   const { pending } = useFormStatus();
   return (
-    <button
-      disabled={pending}
-      className="flex items-center justify-center bg-green-600 text-green-50 rounded px-2 h-9 w-14 py-1"
-    >
-      Add
-    </button>
+    <ShadButton variant="secondary" disabled={pending}>
+      Click me
+    </ShadButton>
   );
 };
